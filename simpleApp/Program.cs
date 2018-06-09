1c1cfd148af688b40136bbe59360a12af5d4bfc0
Wei (Powergun) Ning <macgnw@gmail.com>
2018-06-09 17:40:40 +1000

    - init proj

---
+++ simpleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
